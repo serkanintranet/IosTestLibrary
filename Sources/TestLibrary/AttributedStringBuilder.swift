ce696644442772b5eec545a424c149113275ef2a
serkanintranet <serkan@intranettechnology.com>
2022-10-02 09:20:24 +0300

    Changes

---
+++ Sources/TestLibrary/AttributedStringBuilder.swift
@@ -0,0 +1,34 @@
+//
+//  BurgerShop.swift
+//  
+//
+//  Created by serkan doksöz on 2.10.2022.
+//
+
+import Foundation
+class AttributedStringBuilder {
+    typealias Attributes = [NSAttributedString.Key : Any]
+
+    private let string = NSMutableAttributedString(string: "")
+
+    // We follow the convention of returning the builder object
+    // itself from any configuration methods, and by adding the
+    // @discardableResult attribute we won't get warnings if we
+    // don't end up doing any chaining.
+    @discardableResult
+    func append(_ character: Character,
+                attributes: Attributes) -> AttributedStringBuilder {
+        let addedString = NSAttributedString(
+            string: String(character),
+            attributes: attributes
+        )
+
+        string.append(addedString)
+
+        return self
+    }
+
+    func build() -> NSAttributedString {
+        return NSAttributedString(attributedString: string)
+    }
+}
